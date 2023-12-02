a7fb50ad02ba01fd7ef8f86ad90f62e8162dc17b
Andrei Azhoichyk <azh.andrei@gmail.com>
2023-12-02 22:55:17 +0200

    feat: integrate with backend import-service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://z81yxjs31f.execute-api.eu-north-1.amazonaws.com/dev",
   order: "https://z81yxjs31f.execute-api.eu-north-1.amazonaws.com/dev",
-  import: "https://z81yxjs31f.execute-api.eu-north-1.amazonaws.com/dev",
+  import: "https://tmno4nt3qg.execute-api.eu-north-1.amazonaws.com/dev",
   bff: "https://z81yxjs31f.execute-api.eu-north-1.amazonaws.com/dev",
   cart: "https://z81yxjs31f.execute-api.eu-north-1.amazonaws.com/dev",
 };
